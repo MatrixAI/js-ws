1c1eeaa1f319a5af9b8a87aae51ff204923b8e6c
Amy <50583248+amydevs@users.noreply.github.com>
2023-09-04 18:02:59 +1000

    fix: exported every thing at root on index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ export { default as WebSocketClient } from './WebSocketClient';
 export { default as WebSocketConnection } from './WebSocketConnection';
 export { default as WebSocketStream } from './WebSocketStream';
 
+export * as types from './types';
 export * as utils from './utils';
 export * as events from './events';
 export * as errors from './errors';
+export * as config from './config';
+export * as message from './message';
+
