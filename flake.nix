370dd808e65e4d10d3ec92e5988e56033d2f96ae
Brian Botha <brian.botha@matrix.ai>
2024-09-19 14:35:33 +1000

    fix: fixed problem where `js-ws` failed to install as a dependency in other projects

--- flake.nix
+++ flake.nix
@@ -37,7 +37,6 @@
             mkdir --parents "$(pwd)/tmp"
             export PATH="$(pwd)/dist/bin:$(npm root)/.bin:$PATH"
             npm install --ignore-scripts
-            npx patch-package
             set +v
           '';
         };
